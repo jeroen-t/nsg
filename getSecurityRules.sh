0b5c91ee672acb0fccae7e49816b914001fb142d
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2022-05-12 23:21:31 +0200

    Rm subscriptionid param

--- getSecurityRules.sh
+++ getSecurityRules.sh
@@ -5,7 +5,7 @@ unset resourceGroup
 unset subscriptionId
 
 usage() {
-  echo "Usage: $0 [ -e environment ] [ -n networkSecurityGroup ] [ -r resourceGroup ] [ -s subscriptionId ]" 1>&2 
+  echo "Usage: $0 [ -e environment ] [ -n networkSecurityGroup ] [ -r resourceGroup ]" 1>&2 
 }
 
 exit_abnormal() {
@@ -13,12 +13,11 @@ exit_abnormal() {
   exit 1
 }
 
-while getopts e:n:r:s: flag; do
+while getopts e:n:r: flag; do
     case "${flag}" in
         e) environment=${OPTARG};;
         n) networkSecurityGroup=${OPTARG};;
         r) resourceGroup=${OPTARG};;
-        s) subscriptionId=${OPTARG};;
         *) 
             echo "Invalid option: -$flag" >&2
             exit_abnormal
@@ -29,10 +28,11 @@ done
 shift "$(( OPTIND - 1 ))"
 
 if [ -z "$environment" ] || [ -z "$networkSecurityGroup" ] || [ -z "$resourceGroup" ] || [ -z "$subscriptionId" ]; then
-        echo 'Missing arguments. Please provide values for [e]nvironment, [n]etworkSecurityGroup, [r]esourceGroup and [s]ubscriptionId.' >&2
+        echo 'Missing arguments. Please provide values for [e]nvironment, [n]etworkSecurityGroup and [r]esourceGroup.' >&2
         exit 1
 fi
 
+subscriptionId=$(az account show --query 'id' -o tsv)
 uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/networkSecurityGroups/$networkSecurityGroup?api-version=2021-08-01"
 
 # call nsg api, remove unneeded properties from response and only store the custom security rules
