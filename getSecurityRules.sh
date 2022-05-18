9da28433e0a50445368ad7d1c44112677fecd006
Jeroen Trimbach <jeroen.trimbach@gmail.com>
2022-05-18 20:42:04 +0200

    we

--- getSecurityRules.sh
+++ getSecurityRules.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
-
+set -e
+unset subscriptionId
 unset networkSecurityGroup
 unset resourceGroup
-unset subscriptionId
 
-usage() {
-  echo "Usage: $0 [ -e environment ] [ -n networkSecurityGroup ] [ -r resourceGroup ]" 1>&2 
+function usage() {
+  echo '
+    This program outputs the custom security rules for a given Azure Network Security Group (NSG) in an ARM deployment parameter template.
+
+    -n [networkSecurityGroup] - the Network Security Group name
+    -r [resourceGroup]        - the Resource Group name
+  
+    example: ./'$(basename "$0")' -n Nsg1 -r ResourceGroup1
+  '
 }
 
-exit_abnormal() {
-  usage
-  exit 1
-}
-
-while getopts e:n:r: flag; do
+while getopts 'n:r:' flag; do
     case "${flag}" in
-        e) environment=${OPTARG};;
         n) networkSecurityGroup=${OPTARG};;
         r) resourceGroup=${OPTARG};;
         *) 
-            echo "Invalid option: -$flag" >&2
-            exit_abnormal
+            usage
+            exit 1
             ;;
     esac
 done
 
 shift "$(( OPTIND - 1 ))"
 
-if [ -z "$environment" ] || [ -z "$networkSecurityGroup" ] || [ -z "$resourceGroup" ] || [ -z "$subscriptionId" ]; then
-        echo 'Missing arguments. Please provide values for [e]nvironment, [n]etworkSecurityGroup and [r]esourceGroup.' >&2
+if [ -z "$networkSecurityGroup" ] || [ -z "$resourceGroup" ]; then
+        echo 'WARNING: Missing arguments. Please provide values for [n]etworkSecurityGroup and [r]esourceGroup.' >&2
+        usage
         exit 1
 fi
 
-subscriptionId=$(az account show --query 'id' -o tsv)
-uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/networkSecurityGroups/$networkSecurityGroup?api-version=2021-08-01"
+subscriptionId=$(az account show --query "id" -o tsv)
 
 # call nsg api, remove unneeded properties from response and only store the custom security rules
-secRules=$(az rest --method get --uri $uri | jq 'del(.properties.securityRules[] | .id, .etag, .type, .properties.provisioningState, .properties.priority) | .properties.securityRules')
+uri="https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroup/providers/Microsoft.Network/networkSecurityGroups/$networkSecurityGroup?api-version=2021-08-01"
+securityRules=$(az rest --method get --uri $uri | jq 'del(.properties.securityRules[] | .id, .etag, .type, .properties.provisioningState, .properties.priority) | .properties.securityRules')
+if [ -z "$securityRules" ]; then
+        exit 1
+fi
 
 # add custom security rules to a deployment parameter ARM template
-jq --argjson input "$format" '.parameters.securityRules.value += $input' template.parameters.json > "$environment.parameters.json"
+jq --argjson input "$securityRules" '.parameters.securityRules.value += $input' template.parameters.json > "$networkSecurityGroup.parameters.json"
